1055d7236adb297fd05a2326ae20e4ec9dd79ad7
Tobias <tobias.aditya@gmail.com>
2023-10-14 19:12:56 +0700

    add env var

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+import dotenv
+import os
+dotenv.load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+
 
 app = FastAPI()
 
 @app.get("/test")
 def test():
+    print(f"Envi variable nih : {DB_HOST}")
     return {
         "message" : "Cuma gini aja woi"
     }
