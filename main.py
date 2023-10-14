69e17e31d3da5d6254e1e827a475a1db697a0fe4
Tobias <tobias.aditya@gmail.com>
2023-10-14 20:57:54 +0700

    env variable

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+import dotenv
+import os
+
+dotenv.load_dotenv()
+DB_URI = os.getenv("DB_URI")
 
 app = FastAPI()
 
 @app.get("/test")
 def test():
+
+    print(f"DB URI dari env : {DB_URI}")
     return {
         "message" : "Cuma gini aja OI"
     }
