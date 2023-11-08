4166153e50f37401377402e1101ca25da9ca2dd1
Tobias <tobias.aditya@gmail.com>
2023-11-08 20:42:33 +0700

    add API source and get data

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 import dotenv
 import os
+import requests
 
 dotenv.load_dotenv()
 DB_URI = os.getenv("DB_URI")
+NAME = os.getenv("NAME")
+SOURCE_URL = os.getenv("SOURCE_URL")
 
 app = FastAPI()
 
@@ -14,3 +17,12 @@ def test():
     return {
         "message" : "Cuma gini aja OI"
     }
+
+@app.get("/source")
+def source():
+    return {"source": NAME}
+
+@app.get("/data")
+def get_data():
+    response = requests.get(SOURCE_URL)
+    return response.json()
